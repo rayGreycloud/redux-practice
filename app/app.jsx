ee1bc2797f916e4468ada59e907e5b929e73d721
rayGreycloud <rayGreycloud@gmail.com>
2016-06-14 14:44:20 -0700

    first commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+// load foundation
+$(document).foundation();
+
+// app css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <p>Boilerplate3 Project</p>,
+  document.getElementById("app")
+);
